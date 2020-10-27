8eecf00c116732aa179859718a3cd51b94e83952
veronnah <you@example.com>
2020-10-27 20:52:00 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+const quizData = [
+    {
+        question: 'How old is Florin?',
+        a: '10', 
+        b: '17',
+        c: '26', 
+        d: '110',
+        correct: 'c' 
+    }, 
+    {
+        question: 'What is the most used programming language in 2019?', 
+        a: 'Java', 
+        b: 'C',
+        c: 'Python', 
+        d: 'JavaScript',
+        correct: 'a'
+    }, 
+    {
+        question: 'Who is the president of the US?',
+        a: 'Floring Pop', 
+        b: 'Donald Trump',
+        c: 'Ivan Saldano', 
+        d: 'Mihai Andrei',
+        correct: 'b'
+    }, 
+    {
+        question: 'What does HTML stand for?',
+        a: 'Hypertext Markup language', 
+        b: 'Cascading Style Sheet',
+        c: 'Jason Object Notation', 
+        d: 'Helicopters Terminals Motorboats Lamborginis',
+        correct: 'a'
+    },
+    {
+        question: 'What year was JavaScript launched?',
+        a: '1996', 
+        b: '1995',
+        c: '1994', 
+        d: '1998',
+        correct: 'b'
+    }
+]
