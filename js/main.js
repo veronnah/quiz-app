efbe16ae669373c8ff72349d19fa516946f6dd23
veronnah <you@example.com>
2020-11-06 19:23:32 +0100

    3.0

--- js/main.js
+++ js/main.js
@@ -1,41 +1,40 @@
-const quizData = [
-    {
+const quizData = [{
         question: 'How old is Florin?',
-        a: '10', 
+        a: '10',
         b: '17',
-        c: '26', 
+        c: '26',
         d: '110',
-        correct: 'c' 
-    }, 
+        correct: 'c'
+    },
     {
-        question: 'What is the most used programming language in 2019?', 
-        a: 'Java', 
+        question: 'What is the most used programming language in 2019?',
+        a: 'Java',
         b: 'C',
-        c: 'Python', 
+        c: 'Python',
         d: 'JavaScript',
         correct: 'a'
-    }, 
+    },
     {
         question: 'Who is the president of the US?',
-        a: 'Floring Pop', 
+        a: 'Floring Pop',
         b: 'Donald Trump',
-        c: 'Ivan Saldano', 
+        c: 'Ivan Saldano',
         d: 'Mihai Andrei',
         correct: 'b'
-    }, 
+    },
     {
         question: 'What does HTML stand for?',
-        a: 'Hypertext Markup language', 
+        a: 'Hypertext Markup language',
         b: 'Cascading Style Sheet',
-        c: 'Jason Object Notation', 
+        c: 'Jason Object Notation',
         d: 'Helicopters Terminals Motorboats Lamborginis',
         correct: 'a'
     },
     {
         question: 'What year was JavaScript launched?',
-        a: '1996', 
+        a: '1996',
         b: '1995',
-        c: '1994', 
+        c: '1994',
         d: '1998',
         correct: 'b'
     }
@@ -43,7 +42,10 @@ const quizData = [
 
 let currentQuestion = 0;
 
+const answerELs = document.querySelectorAll(".answer");
+const quiz = document.getElementById("quiz")
 let questionEl = document.getElementById("question");
+
 let a_text = document.getElementById("a_text");
 let b_text = document.getElementById("b_text");
 let c_text = document.getElementById("c_text");
@@ -51,30 +53,63 @@ let d_text = document.getElementById("d_text");
 let submitBtn = document.getElementById("submit");
 
 let currentQuiz = 0;
+let score = 0;
+let answer;
 
 loadQuiz();
 
-function loadQuiz(){
+function loadQuiz() {
+    deselectAnswers();
+
     const currentQuizData = quizData[currentQuiz];
+
     questionEl.innerText = currentQuizData.question;
     a_text.innerText = currentQuizData.a;
     b_text.innerText = currentQuizData.b;
     c_text.innerText = currentQuizData.c;
     d_text.innerText = currentQuizData.d;
 
-    
+
 }
 
+function getSelected() {
+   
+
+    let answer = undefined; 
+
+    answerELs.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+    return answer;
+}
+
+function deselectAnswers() {
+    const answerELs = document.querySelectorAll(".answer");
+
+    answerELs.forEach(answerEl => { 
+        answerEl.checked = false;
+    });
+}
+
+
 submitBtn.addEventListener('click', () => {
-    currentQuiz++;
+    //check to see the answer 
+    const answer = getSelected();
+   
 
-    if(currentQuiz < quizData.length){
+    if (answer) {
+        if( answer === quizData[currentQuiz].correct){
+            score++;
+        }
+        currentQuiz++;
+    if (currentQuiz < quizData.length) {
         loadQuiz();
-    } 
-    else {
-        // TODO: show results 
-        alert('You finished! Get yourself an Orange lemonade');
+    } else {
+       
+       quiz.innerHTML = `<h2>You answered correctly at ${score}/ ${quizData.length} questions.</h2>`;
     }
-
-    loadQuiz();
+    }
+    
 });
